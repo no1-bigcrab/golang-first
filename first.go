270267ec8f5ae967956f9d45b9b7aeafd1d09af1
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-13 15:03:48 +0700

    first commit

---
+++ first.go
@@ -0,0 +1,22 @@
+// Go program to illustrate the concept
+// of dynamic values and types
+package main
+
+import "fmt"
+
+// Creating an interface
+func myInterface(a interface{}) {
+	value, ok := a.(float64)
+	fmt.Println(value, ok)
+}
+func main() {
+	var a1 interface {
+	} = 98.09
+
+	myInterface(a1)
+
+	var a2 interface {
+	} = "Germa 66"
+
+	myInterface(a2)
+}
