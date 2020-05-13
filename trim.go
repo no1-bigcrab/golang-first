a9683375ac1444f83c45dffc7f7bba5fa859eefa
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-13 15:26:19 +0700

    add trim golang

--- trim.go
+++ trim.go
@@ -22,6 +22,7 @@ func main() {
 	// Using Trim() function
 	res1 := strings.Trim(str1, "!")
 	res2 := strings.Trim(str2, "@$")
+
 	// Trimming the given strings
 	// Using TrimLeft() function
 	res3 := strings.TrimLeft(str1, "!*")
@@ -31,6 +32,16 @@ func main() {
 	res5 := strings.TrimRight(str1, "!*")
 	res6 := strings.TrimRight(str2, "$")
 
+	// Trimming suffix string from the given strings
+	// Using TrimSuffix() function
+	res7 := strings.TrimSuffix(str1, "GeeksforGeeks")
+	res8 := strings.TrimSuffix(str2, "Hello")
+
+	// Trimming prefix string from the given strings
+	// Using TrimPrefix() function
+	res9 := strings.TrimPrefix(str1, "Welcome")
+	res10 := strings.TrimPrefix(str2, "Hello")
+
 	// Displaying the results
 	fmt.Println("\nStrings after trimming:")
 	fmt.Println("Result 1: ", res1)
@@ -39,4 +50,8 @@ func main() {
 	fmt.Println("Result 4:", res4)
 	fmt.Println("Result 5: ", res5)
 	fmt.Println("Result 6:", res6)
+	fmt.Println("Result 7: ", res7)
+	fmt.Println("Result 8:", res8)
+	fmt.Println("Result 7: ", res9)
+	fmt.Println("Result 8:", res10)
 }
