42470a8757c7598ab7420089b14bf278dbd4ddf5
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-14 13:48:35 +0700

    update thêm sửa xoá api the first golang

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -45,31 +47,57 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 //get single book by id
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r);// get params send by url
-	// 
+	params := mux.Vars(r) // get params send by url
+	//
 	// loop
-	for _, item:= range books {
-		if item.ID == params["id"]  {
-			json.NewEncoder(w).Encode(item);
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Book{});
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 //create book in the table
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000)) ///mock id
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 //update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"] ///mock id
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 //delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get params send by url
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func handleRequests() {
@@ -77,30 +105,30 @@ func handleRequests() {
 	myRoute := mux.NewRouter().StrictSlash(true)
 
 	//Mock data
-	books = append( books, Book{
+	books = append(books, Book{
 		ID:    "1",
-		Isbn:   "44873",
+		Isbn:  "44873",
 		Title: "Tiếng gọi nơi hoang dã",
 		Author: &Author{
 			FirstName: "John Biden",
-			LastName: "Bidonn",
-			Age:      45,
-			Address:  "46 Khâm Thiên",
-			Phone:    "0839828648",
-			Email:    "banhbao01@mail.com",
+			LastName:  "Bidonn",
+			Age:       45,
+			Address:   "46 Khâm Thiên",
+			Phone:     "0839828648",
+			Email:     "banhbao01@mail.com",
 		},
 	})
-	books = append( books, Book {
+	books = append(books, Book{
 		ID:    "2",
-		Isbn:   "44321",
+		Isbn:  "44321",
 		Title: "1977 Vlogs",
 		Author: &Author{
 			FirstName: "Chí Biden",
-			LastName: "Phèo",
-			Age:      40,
-			Address:  "Làng Vũ Đại",
-			Phone:    "0937275762",
-			Email:    "chidepzai01@mail.com",
+			LastName:  "Phèo",
+			Age:       40,
+			Address:   "Làng Vũ Đại",
+			Phone:     "0937275762",
+			Email:     "chidepzai01@mail.com",
 		},
 	})
 
