cda9f1c1e0a28f745e24cdb2f5d4a194163046d8
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-14 17:10:07 +0700

    router of golang with gorilla/mux Router

--- main.go
+++ main.go
@@ -3,15 +3,59 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You've requested the book:")
+}
+
+func ReadBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You've requested the book:")
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You've requested the book:")
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You've requested the book:")
+}
+
+func BookHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You've requested the book:")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
+	r := mux.NewRouter()
+
+	//http Methods
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+	//domain và host
+	r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com")
+
+	//schemes router
+	r.HandleFunc("/secure", BookHandler).Schemes("https")
+	r.HandleFunc("/insecure", BookHandler).Schemes("http")
+
+	//subrouter
+	bookrouter := r.PathPrefix("/books").Subrouter()
+	bookrouter.HandleFunc("/", BookHandler)
+	bookrouter.HandleFunc("/{title}", BookHandler)
+
+	// function cơ bản
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":8081", r)
 }
