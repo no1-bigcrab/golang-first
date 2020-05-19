518b7405817166288c1f324962426c71e383eb6d
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-19 08:48:36 +0700

    cbs

--- src/serve/main.go
+++ src/serve/main.go
@@ -1,39 +1,56 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	"fmt"
+// 	"log"
+
+// 	_ "github.com/lib/pq"
+// )
+
+// func main() {
+// 	db, err := sql.Open("postgres", "user=macosx password=123 dbname=testdb sslmode=disable")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer db.Close()
+// 	if err != nil {
+// 		// handle this error better than this
+// 		panic(err)
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var id int
+// 		var name string
+// 		err = rows.Scan(&id, &name)
+// 		if err != nil {
+// 			// handle this error
+// 			panic(err)
+// 		}
+// 		fmt.Println(id, name)
+// 	}
+// 	// get any error encountered during iteration
+// 	err = rows.Err()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
-	db, err := sql.Open("postgres", "user=macosx password=123 dbname=testdb sslmode=disable")
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Hello world::\n")
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=macosx dbname=testdb password=123")
 	defer db.Close()
-
-	rows, err := db.Query("SELECT id, name FROM users LIMIT $1", 3)
 	if err != nil {
-		// handle this error better than this
-		panic(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		err = rows.Scan(&id, &name)
-		if err != nil {
-			// handle this error
-			panic(err)
-		}
-		fmt.Println(id, name)
-	}
-	// get any error encountered during iteration
-	err = rows.Err()
-	if err != nil {
-		panic(err)
+		fmt.Println("Fail")
+	} else {
+		fmt.Println("Success")
 	}
 }
