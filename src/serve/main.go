04aea11adb9438118606846af5503c028090cc43
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-21 11:26:31 +0700

    api login register

--- src/serve/main.go
+++ src/serve/main.go
@@ -1,11 +1,27 @@
+// main.go
 package main
 
 import (
-	"fmt"
-	"serve/packtest"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"serve/api/controllers"
 )
 
 func main() {
-	fmt.Printf("using mod1")
-	packtest.PackPrint()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	app := controllers.App{}
+	app.Initialize(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"))
+
+	app.RunServer()
 }
